083e898c63d5ae56b6c902d3cd612b37e2b40af6
rhoang57 <rhoang57@gmail.com>
2018-12-16 23:35:29 -0800

    successfully connecting to server-basic html created-need compatibility logic added to apiRoutes.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,1 +1,30 @@
 //You should save your application's data inside of app/data/friends.js as an array of objects. Each of these objects should roughly follow the format below.
+
+// ===============================================================================
+// DATA
+// Below data will hold all of the survery results from potential friends.
+// Initially set dummy friend as Slimer.
+// The array you could have been set to an empty array as well.
+// ===============================================================================
+
+var friendsArray = [
+    {
+      customerName: "Slimer",
+      photo: "https://cdn.shopify.com/s/files/1/1452/7530/products/slimer.jpg?v=1505206859",
+      scores: [
+          5,
+          3,
+          1,
+          3,
+          5,
+          5,
+          5,
+          5,
+          1,
+          3
+      ]
+    }
+  ];
+  
+  // Export the "dummy" array of Slimer to be default. Below line makes the friendsArray it accessible to other files using require and list friends.js as the required file.
+  module.exports = friendsArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,2 +1,41 @@
 //A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
 //A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information such as: name, link to photo, and array or survery answers.
+// ===============================================================================
+
+var friendsData = require("../data/friends");
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In the below case when a user visits a link
+  // (ex: localhost:PORT/api/friends... they are shown a JSON of all possible friend matches)
+  // ---------------------------------------------------------------------------
+
+  app.get("/api/friends", function(req, res) {
+    res.json(friendsData);
+  });
+
+  // API POST Requests
+  // Below code handles when a user submits a form and thus submits data to the server.
+  // In each of the below cases, when a user submits form data (a JSON object)
+  // ...the JSON is pushed to the appropriate JavaScript array
+  // (ex. User fills out a reservation request... this data is then sent to the server...
+  // Then the server saves the data to the tableData array)
+  // ---------------------------------------------------------------------------
+
+  app.post("/api/friends", function(req, res) {
+    // The compatibility logic will go here.
+   /*
+    */
+
+};
