7a16970175a16c95e5354243e776e8b5f4b3bcdc
rhoang57 <rhoang57@gmail.com>
2018-12-19 03:32:44 -0800

    added another dummy profile to friends.js file

--- app/data/friends.js
+++ app/data/friends.js
@@ -8,23 +8,39 @@
 // ===============================================================================
 
 var friendsArray = [
-    {
-      name: "Slimer",
-      photo: "https://cdn.shopify.com/s/files/1/1452/7530/products/slimer.jpg?v=1505206859",
-      scores: [
-          5,
-          3,
-          1,
-          3,
-          5,
-          5,
-          5,
-          5,
-          1,
-          3
-      ]
-    }
-  ];
-  
-  // Export the "dummy" array of Slimer to be default. The line below makes the friendsArray accessible to other files using require and list friends.js as the required file.
-  module.exports = friendsArray;
+  {
+    name: "Slimer",
+    photo: "https://cdn.shopify.com/s/files/1/1452/7530/products/slimer.jpg?v=1505206859",
+    scores: [
+      5,
+      3,
+      1,
+      3,
+      5,
+      5,
+      5,
+      5,
+      1,
+      3
+    ]
+  },
+  {
+    name: "Barack Obama",
+    photo: "https://pbs.twimg.com/profile_images/822547732376207360/5g0FC8XX_400x400.jpg",
+    scores: [
+      5,
+      5,
+      5,
+      5,
+      4,
+      5,
+      5,
+      5,
+      1,
+      1
+    ]
+  }
+]
+
+// Export the "dummy" array of Slimer to be default. The line below makes the friendsArray accessible to other files using require and list friends.js as the required file.
+module.exports = friendsArray;
